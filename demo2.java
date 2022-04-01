0c2aee8796166c7c50cc37d6694cbf06dc5b8c76
Aryaman Singla <aryamansingla199@gmail.com>
2022-04-01 16:34:57 +0530

    demo2.java is modified

--- demo2.java
+++ demo2.java
@@ -1,1 +1,19 @@
- new file
+public class Aryaman{
+    String Name;
+    int age;
+    static void setName(String name){
+        this.Name = name;
+        
+    }
+    static String getName(){
+        return Name;
+    }
+    
+}
+public class Main{
+    public static int main(String [] args){
+        System.out.println("My name is aryaman");
+        Aryaman me = new Aryaman();
+        
+    }
+}
